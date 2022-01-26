425a395a58f19343cbe5d3fb7ad7fcccb2776f23
rohit-sawant <sawant.rohit510@gmail.com>
2022-01-26 23:37:21 +0530

    Intial commit

---
+++ ShopmeProject/ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/site/MainController.java
@@ -0,0 +1,11 @@
+package com.shopme.site;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+public class MainController {
+	
+	@GetMapping("")
+	public String viewHomePage() {
+		return "index";
+	}
+}
